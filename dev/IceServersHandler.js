0bfba0cc51ce4fb04f432d71a3aba7e3cd4efdc3
zebby-nick <50813055+zebby-nick@users.noreply.github.com>
2020-07-30 15:23:48 +0800

    Fix stun

--- dev/IceServersHandler.js
+++ dev/IceServersHandler.js
@@ -6,7 +6,12 @@ var IceServersHandler = (function() {
         // pions: 7575
         var iceServers = [{
             'urls': [
-                'stun:10.179.6.106:3478?transport=udp'
+                'stun:10.179.6.101:3478?transport=udp',
+                'stun:10.179.6.102:3478?transport=udp',
+                'stun:10.179.6.103:3478?transport=udp',
+                'stun:10.179.6.104:3478?transport=udp',
+                'stun:10.179.6.105:3478?transport=udp',
+                'stun:10.179.6.106:3478?transport=udp',
             ]
         }];
 
